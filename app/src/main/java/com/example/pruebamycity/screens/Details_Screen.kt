a961aec9288b974df020c8d76dbcc5e3360f00a7
andrea44444 <andreaolcakova@gmail.com>
2023-11-19 13:46:00 +0100

    mejorar diseño y mostrar descripcion

--- app/src/main/java/com/example/pruebamycity/screens/Details_Screen.kt
+++ app/src/main/java/com/example/pruebamycity/screens/Details_Screen.kt
@@ -1,20 +1,20 @@
 package com.example.pruebamycity.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -33,14 +33,12 @@ fun Details_Screen(navController: NavHostController) {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        contentAlignment = Alignment.Center
+        //contentAlignment = Alignment.Center
     ){
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxHeight()
                 .padding(start = 25.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.Start
         ) {
             Image(
                 modifier = Modifier
@@ -50,14 +48,27 @@ fun Details_Screen(navController: NavHostController) {
                 contentDescription = null
             )
 
-            Text(
-                fontSize = 20.sp,
-                text= recomendacion.nombreRecomendacion
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(top = 25.dp),
+                horizontalAlignment = Alignment.Start
+            ){
+                Text(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    text= recomendacion.nombreRecomendacion
+                )
 
-            Text(
-                text= recomendacion.direccion
-            )
+                Text(
+                    text= recomendacion.direccion
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text= recomendacion.descripcion
+                )
+            }
+
         }
     }
 }
